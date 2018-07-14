a05aa4acab3e4a484e44e4991ca1c5bba95f6fce
David Ostrovsky <david@ostrovsky.org>
2018-07-14 14:32:06 +0200

    KafkaProperties: Use Class.getName() idiom for serializer registration
    Change-Id: I8d17e0483ecdfe9a207e0a12c7910aba4799abae

--- src/main/java/com/googlesource/gerrit/plugins/kafka/config/KafkaProperties.java
+++ src/main/java/com/googlesource/gerrit/plugins/kafka/config/KafkaProperties.java
@@ -19,9 +19,14 @@ import com.google.gerrit.extensions.annotations.PluginName;
 import com.google.gerrit.server.config.PluginConfig;
 import com.google.gerrit.server.config.PluginConfigFactory;
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+@Singleton
 public class KafkaProperties extends java.util.Properties {
   private static final long serialVersionUID = 0L;
+
+  public static final String KAFKA_STRING_SERIALIZER = StringSerializer.class.getName();
 
   private final String topic;
 
@@ -40,8 +45,8 @@ public class KafkaProperties extends java.util.Properties {
     put("batch.size", 16384);
     put("linger.ms", 1);
     put("buffer.memory", 33554432);
-    put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-    put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    put("key.serializer", KAFKA_STRING_SERIALIZER);
+    put("value.serializer", KAFKA_STRING_SERIALIZER);
   }
 
   private void applyConfig(PluginConfig config) {
