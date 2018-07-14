8c79af13083af9ff15c129fdacc309469146855f
David Ostrovsky <david@ostrovsky.org>
2018-07-14 08:45:09 +0200

    Rename message package in publisher
    Change-Id: I4462361b6b614cabf182eced0c7312258e7c771a

--- src/main/java/com/googlesource/gerrit/plugins/kafka/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/kafka/Module.java
@@ -20,8 +20,8 @@ import com.google.gerrit.server.events.EventListener;
 import com.google.gson.Gson;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
-import com.googlesource.gerrit.plugins.kafka.message.GsonProvider;
-import com.googlesource.gerrit.plugins.kafka.message.Publisher;
+import com.googlesource.gerrit.plugins.kafka.publisher.GsonProvider;
+import com.googlesource.gerrit.plugins.kafka.publisher.Publisher;
 
 class Module extends AbstractModule {
 
